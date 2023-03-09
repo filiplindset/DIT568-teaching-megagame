f3f50a1d5e626941ec1e19352e64c57d3f83b94c
blixtcorp <jakobpersson007@gmail.com>
2023-03-09 14:09:00 +0100

    crude resourcelist

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div id="app">
+      <ResourceList :resources="resources"/>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  </template>
+  
+  <script>
+  import ResourceList from './components/ResourceList.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      ResourceList
+    },
+    data() {
+      return {
+        resources: [
+          { id: 1, name: 'Wood', count: 300 },
+          { id: 2, name: 'Iron', count: 500 },
+          { id: 3, name: 'Food', count: 10 },
+        ]
+      }
+    }
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+  </script>
+  
---
+++ src/components/ResourceList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h2>Resources</h2>
+      <ul>
+        <li v-for="resource in resources" :key="resource.id" @click="setCount(resource)">
+          <a>{{ resource.name }}: {{ resource.count }}</a>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      resources: {
+        type: Array,
+        required: true
+      }
+    },
+    methods: {
+        setCount(resource){
+            resource.count+= 10
+        }
+    }
+  }
+  </script>
+  
