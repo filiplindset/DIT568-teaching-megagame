94de88ab4681fa415da6025fc07ac609e1ad3dde
blixtcorp <jakobpersson007@gmail.com>
2023-03-29 15:16:11 +0200

    create a player view that uses the resourcelist component to display resources and their quantities

---
+++ src/views/PlayerView.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="player-view">
+        <h1>Player View</h1>
+        <ResourceList :resources="resources" />
+    </div>
+</template>
+
+<script>
+import ResourceList from 'src/components/ResourceList.vue'
+
+export default {
+    name: 'PlayerView',
+    components: {
+        ResourceList
+    },
+    data() {
+        return {
+            // some placeholder resources
+            resources: [
+                { name: 'Wood', quantity: 10 },
+                { name: 'Stone', quantity: 5 },
+                { name: 'Iron', quantity: 2 },
+                { name: 'Gold', quantity: 1 }
+            ]
+        }
+    }
+}
+</script>
+
