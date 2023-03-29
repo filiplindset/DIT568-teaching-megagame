037c2c053e78a742bd94613a0270fa92d40532db
blixtcorp <jakobpersson007@gmail.com>
2023-03-29 15:15:34 +0200

    create component holding resources

---
+++ src/components/ResourceList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="resource-list">
+        <div v-for="(resource, index) in resources" :key="index" class="resource">
+            <span class="name">{{ resource.name }}</span>
+            <span class="quantity">{{ resource.quantity }}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ResourceList',
+    props: {
+        resources: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.resource-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+.resource {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;
+    padding: 4px 8px;
+    background-color: #eee;
+    border-radius: 4px;
+}
+.name {
+    margin-right: 8px;
+    font-weight: bold;
+}
+</style>
