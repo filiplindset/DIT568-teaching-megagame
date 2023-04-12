dae68d132b0346132a8ba74b46c0defe4281c755
blixtcorp <jakobpersson007@gmail.com>
2023-04-12 13:49:50 +0200

    add a grid component and some styling for it

---
+++ src/components/TeamSelect.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div class="grid-container">
+            <div v-for="item in items" :key="item.id" @click="selectItem(item)" :class="{ 'selected': selectedItem === item }" class="grid-item">
+                {{ item.name }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TeamSelect",
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    data() {
+        return {
+            selectedItem: null
+        };
+    },
+    methods: {
+        selectItem(item) {
+            this.selectedItem = item;
+            this.$emit('item-selected', item);
+        }
+    }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -2,16 +2,19 @@
     <div class="player-view">
         <h1>Player View</h1>
         <ResourceList :resources="resources" />
+        <TeamSelect :items="items" @item-selected="onItemSelect"></TeamSelect>
     </div>
 </template>
 
 <script>
 import ResourceList from "@/components/ResourceList.vue";
+import TeamSelect from "@/components/TeamSelect.vue";
 
 export default {
     name: 'PlayerView',
     components: {
-        ResourceList
+        ResourceList,
+        TeamSelect
     },
     data() {
         return {
@@ -21,7 +24,20 @@ export default {
                 { name: 'Stone', quantity: 5 },
                 { name: 'Iron', quantity: 2 },
                 { name: 'Gold', quantity: 1 }
+            ],
+            // teams/factions
+            items: [
+                { id: 1, name: 'Item 1' },
+                { id: 2, name: 'Item 2' },
+                { id: 3, name: 'Item 3' },
+                { id: 4, name: 'Item 4' },
+                { id: 5, name: 'Item 5' }
             ]
+        };
+    },
+    methods: {
+        onItemSelect(item) {
+            console.log('Selected item:', item);
         }
     }
 }
