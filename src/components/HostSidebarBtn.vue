d21c39336ee7df15b5d7359126438d6bb5faef17
Filip Lindset <lindset@chalmers.se>
2023-04-13 11:04:32 +0200

    Basic host implementation

---
+++ src/components/HostSidebarBtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <img :src="imageSrc" @click="redirectToUrl" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HostSidebarBtn',
+  props: {
+    imageSrc: {
+      type: String,
+      required: true
+    },
+    redirectUrl: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    redirectToUrl() {
+      window.location.href = this.redirectUrl;
+    }
+  }
+}
+</script>
