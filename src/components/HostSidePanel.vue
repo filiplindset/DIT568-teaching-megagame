053f431c3cdbc80eb03d4789339e56bfac782b63
Filip Lindset <lindset@chalmers.se>
2023-04-19 11:53:44 +0200

    Moderator page update

--- src/components/HostSidePanel.vue
+++ src/components/HostSidePanel.vue
@@ -1,11 +1,59 @@
 <template>
   <div class="side-panel">
     <div class="icon-container">
-      <img src='src/components/icons/vilt.png' @click="changeView('timer')"/>
-      <img src='src/components/icons/vilt.png' @click="changeView('impersonation')"/>
+      <div class="icon">
+        <img src='src/components/icons/timer.png' @click="changeView('timer')"/>
+        <div class="icon-text">Timer</div>
+      </div>
+      <div class="icon">
+        <img src='src/components/icons/moderation.png' @click="changeView('impersonate')"/>
+        <div class="icon-text">Moderation</div>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+.side-panel {
+  position: fixed;
+  top: 40px;
+  left: 0;
+  bottom: 0;
+  width: 75px;
+  background-color: #fa6607;
+  z-index: 999;
+}
+
+.icon-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  height: 100%;
+  padding-top: 10px;
+}
+
+.icon {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.icon img {
+  width: 58px;
+  height: 58px;
+  margin-bottom: 5px;
+}
+
+.icon-text {
+  font-size: 15px;
+  text-align: center;
+  color: #444;
+}
+</style>
+
 
 <script>
 export default {
@@ -21,27 +69,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.side-panel {
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: 50px;
-  background-color: #f1f1f1;
-  z-index: 999;
-}
-
-.icon-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-}
-
-.icon {
-  margin-bottom: 10px;
-}
-</style>
